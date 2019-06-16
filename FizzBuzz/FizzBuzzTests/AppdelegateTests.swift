382932bed5db858dce89f08a716d0c300e3f3d0c
Kylta <Christophe.bugnon02@gmail.com>
2019-06-16 12:46:36 +0200

    Made  conform to

---
+++ FizzBuzz/FizzBuzzTests/AppdelegateTests.swift
@@ -0,0 +1,9 @@
+//
+//  AppdelegateTests.swift
+//  FizzBuzzTests
+//
+//  Created by Christophe Bugnon on 16/06/2019.
+//  Copyright © 2019 Christophe Bugnon. All rights reserved.
+//
+
+import Foundation
