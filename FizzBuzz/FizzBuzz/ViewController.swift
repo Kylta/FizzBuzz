03743ed3f961c23745898d9b024eea74806a8e52
Kylta <Christophe.bugnon02@gmail.com>
2019-06-16 11:18:32 +0200

    Create FizzBuzz application

---
+++ FizzBuzz/FizzBuzz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FizzBuzz
+//
+//  Created by Christophe Bugnon on 16/06/2019.
+//  Copyright © 2019 Christophe Bugnon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
